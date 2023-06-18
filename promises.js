e85fe40e381397f8d144544896bd7a3685d1b6a1
Michael J Maggs <seattleslew@runawayserver.com>
2023-06-17 19:23:47 -0700

    examples of Promise.all + status of pending

--- promises.js
+++ promises.js
@@ -6,6 +6,23 @@ const promise = new Promise( ( resolve, reject ) => {
    }
 } );
 
+const promise2 = new Promise( ( resolve, reject ) =>
+   setTimeout( resolve, 100, 'HIII' )
+);
+
+const promise3 = new Promise( ( resolve, reject ) =>
+   setTimeout( resolve, 1000, 'POOKIE' )
+);
+
+const promise4 = new Promise( ( resolve, reject ) =>
+   setTimeout( resolve, 3000, 'Is it me you are looking for?' )
+);
+
+Promise.all( [ promise, promise2, promise3, promise4 ] )
+   .then( values => {
+      console.log( values );
+   } );
+
 // example of chaining and throw/catch
 promise
    .then( result => {
@@ -16,4 +33,19 @@ promise
    .catch( () => console.log( 'error!' ) )
    .then( result3 => {
       console.log( result3 + '!' );
-   } ); 
+   } );
+
+
+const urls = [
+   'https://jsonplaceholder.typicode.com/users',
+   'https://jsonplaceholder.typicode.com/posts',
+   'https://jsonplaceholder.typicode.com/albums'
+];
+
+Promise.all( urls.map( url => {
+   return fetch( url ).then( resp => resp.json() );
+} ) ).then( results => {
+   console.log( results[ 0 ] );
+   console.log( results[ 1 ] );
+   console.log( results[ 2 ] );
+} ).catch( () => console.log( 'error' ) );
