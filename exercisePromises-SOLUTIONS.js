62c2af45eab9cc0abab80315cf34f220ecf98f67
Michael J Maggs <seattleslew@runawayserver.com>
2023-06-18 22:26:08 -0700

    exercises solved

--- exercisePromises-SOLUTIONS.js
+++ exercisePromises-SOLUTIONS.js
@@ -1,30 +1,28 @@
 // Solve the questions below:
 
 // #1) Create a promise that resolves in 4 seconds and returns "success" string
-const four = new Promise( ( resolve, reject ) => {
-  setTimeout( resolve, 4000, "success" );
-}
-);
+const promise = new Promise( ( resolve, reject ) => {
+  setTimeout( () => {
+    resolve( "success" );
+  }, 4000 );
+} );
 
 // #2) Run the above promise and make it console.log "success"
-four.then( console.log );
-
-four.then( val => console.log( val ) );
+promise.then( console.log );
+// or
+promise.then( resp => console.log( resp ) );
 
 // #3) Read about Promise.resolve() and Promise.reject(). How can you make
 // the above promise shorter with Promise.resolve() and console loggin "success"
-
-const newPromise = Promise.resolve(
+const promise = Promise.resolve(
   setTimeout( () => {
-    4000;
-  }, console.log( "success" ) )
+    console.log( "success" );
+  }, 4000 )
 );
 
 // #4) Catch this error and console log 'Ooops something went wrong'
-// Promise.reject( 'failed' );
-
 Promise.reject( 'failed' )
-  .catch( err => console.log( 'Ooops, something went wrong' ) );
+  .catch( console.log( 'Ooops something went wrong' ) );
 
 // #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
 // Console.log the output and make sure it has a catch block as well.
@@ -35,11 +33,16 @@ const urls = [
   'http://swapi.dev/api/people/4'
 ];
 
-Promise.all( urls.map( url => {
-  return fetch( url ).then( url => url.json() );
-} ) )
-  .then( obj => console.log( obj ) )
-  .catch( err => console.log( "error", err ) );
+Promise.all( urls.map( url =>
+  fetch( url ).then( people => people.json() )
+) )
+  .then( array => {
+    console.log( '1', array[ 0 ] );
+    console.log( '2', array[ 1 ] );
+    console.log( '3', array[ 2 ] );
+    console.log( '4', array[ 3 ] );
+  } )
+  .catch( err => console.log( 'ughhhh fix it!', err ) );
 
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle it?
